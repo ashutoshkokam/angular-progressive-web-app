6f30e36da266fc2a39ffa9345b83f44bd07a71cf
mi_ashutosh <ashutoshkokam@outlook.com>
2020-05-31 13:09:37 +0530

    Added Spinner service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { PostComponent } from './post/post.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
-import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
+import { PostComponent } from "./post/post.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatCardModule } from "@angular/material/card";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { SpinnerHttpInterceptor } from "./spinner-interceptor.service";
+import { SpinnerComponent } from "./spinner/spinner.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostComponent
-  ],
+  declarations: [AppComponent, PostComponent, SpinnerComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatCardModule,
     MatDividerModule,
     HttpClientModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    BrowserAnimationsModule
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production,
+    }),
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: SpinnerHttpInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
